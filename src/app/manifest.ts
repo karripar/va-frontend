72dfc3642566ca521b50f835e91d6a6c7685c379
karripartanen <karripartanen25@gmail.com>
2025-09-27 19:54:27 +0300

    some work

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,17 +2,22 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
     return {
-        name: 'Vaihtoaktivaattori', // TODO: Change this when the name has been decided
+        name: 'Vaihtoaktivaattori', // TODO: Change this when the final name has been decided
         short_name: 'Vaihtoaktivaattori',
         description: 'Platform for students interested in student exchange.',
         start_url: '/',
         display: 'standalone',
         background_color: '#ffffff',
-        theme_color: 'FF5000',
+        theme_color: '#FF5000',
         icons: [
             {
                 src: '', // TODO: Add icons to public folder and update paths
                 sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                src: '',
+                sizes: '256x256',
                 type: 'image/png',
             },
             {
