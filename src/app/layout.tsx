47742a9332dc116a8b8a6cca011d87a3a557c264
unknown <uweraodette01@gmail.com>
2025-10-07 17:55:52 +0300

    more corrections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,21 @@
+"use client";
 import "./globals.css";
 import Navbar from "../components/NavBar";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Vaihtoaktivaattori",
-  description: "Platform for students interested in student exchange.",
-  manifest: "/manifest.json", // points to your manifest
-  icons: {
-    icon: "/favicon.ico",
-    apple: [
-      { url: "/app-icon-192x192.png", sizes: "192x192", type: "image/png" },
-      { url: "/app-icon-512x512.png", sizes: "512x512", type: "image/png" },
-    ],
-  },
-};
-
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+  const isProfilePage = pathname?.startsWith("/profile");
+
   return (
     <html lang="fi">
       <body className={` antialiased`}>
-        <Navbar />
-        <main className="mx-auto max-w-full mb-10">{children}</main>
+        {!isProfilePage && <Navbar />}
+        <main className="mx-auto max-w-full">{children}</main>
       </body>
     </html>
   );
