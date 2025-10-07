24ec1c5986f4c21bc9ea5c76f3d0b8d4cab47e9e
unknown <uweraodette01@gmail.com>
2025-10-07 15:01:38 +0300

    Updated the placeholder test to match current home page content

--- src/tests/Testing.test.tsx
+++ src/tests/Testing.test.tsx
@@ -1,9 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import Home from "../app/page";
-import DestinationsPage from "../app/destinations/page";
 
 test("placeholder test for using github actions", () => {
   render(<Home />);
-  const linkElement = screen.getByText(/Get started by editing/i);
+  const linkElement = screen.getByText(/Tervetuloa Metropolian/i);
   expect(linkElement).toBeDefined();
 });
