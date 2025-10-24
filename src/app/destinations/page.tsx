bc9d149746ce0e3dd7569a222b41856e0bc6847d
karripartanen <karripartanen25@gmail.com>
2025-10-24 14:44:25 +0300

    some tests for destinations

--- src/app/destinations/page.tsx
+++ src/app/destinations/page.tsx
@@ -4,7 +4,11 @@ import React, { useState } from "react";
 import DestinationList from "@/components/exchange-destinations/DestinationList";
 import Image from "next/image";
 
-const DestinationMap = React.lazy(() => import("@/components/exchange-destinations/DestinationMap"));
+// const DestinationMap = React.lazy(() => import("@/components/exchange-destinations/DestinationMap"));
+
+// Normal import for testing purposes, vitest has issues with React.lazy
+import DestinationMap from "@/components/exchange-destinations/DestinationMap";
+
 
 const fieldLabels: Record<string, string> = {
   tech: "Tekniikka",
