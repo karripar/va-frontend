956bf91e871012d1ef3f0e5354db3851b9d20018
karripartanen <karripartanen25@gmail.com>
2025-11-16 22:51:12 +0200

    fix to import

--- src/hooks/destinationHooks.ts
+++ src/hooks/destinationHooks.ts
@@ -1,7 +1,7 @@
 "use client";
 import fetchData from "@/lib/fetchData";
 import { useEffect, useState } from "react";
-import { DestinationWithCoordinatesResponse, ProfileResponse } from "va-hybrid-types/";
+import { DestinationWithCoordinatesResponse, ProfileResponse } from "va-hybrid-types/contentTypes";
 
 // Temporary types until they are added to va-hybrid-types
 interface ApplicationsResponse {
